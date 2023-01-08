b5c260b54de40dcd2a7451145a6f19e704b1c68e
Prince-OFlynn <thedangames8@gmail.com>
2023-01-08 20:43:20 +0300

    23

--- shinigami.js
+++ shinigami.js
@@ -1,13 +1,1 @@
 console.log("from script file");
-function toggleMe(a) {
-   var e = document.getElementByClassName("collapsible");
-   if(!e) return true;
-
-   if(e.style.display == "none") {
-      e.style.display = "block"
-   }
-   else {
-      e.style.display = "none"
-   }
-   return true;
-}
