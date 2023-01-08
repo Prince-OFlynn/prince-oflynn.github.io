122b443f2c57bd7a23b372da1599035e74b38487
Prince-OFlynn <thedangames8@gmail.com>
2023-01-08 20:49:35 +0300

    24

--- shinigami.js
+++ shinigami.js
@@ -1,1 +1,16 @@
 console.log("from script file");
+function toggleMe(a) {
+   var e = document.getElementById(a);
+   var toggleIcon = document.getElementById('toggle-icon');
+   if(!e) return true;
+
+   if(e.style.display == "none") {
+      e.style.display = "block";
+      toggleIcon.innerHTML = '-';
+   }
+   else {
+      e.style.display = "none";
+      toggleIcon.innerHTML = '+';
+   }
+   return true;
+}
